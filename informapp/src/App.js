f357562b20f0c7b1f1b908ecdcaf0b7c2f16d2ba
iwangy <35878784+iwangy@users.noreply.github.com>
2021-05-24 18:23:39 -0700

    re-create react app, import bootstrap, split components

--- informapp/src/App.js
+++ informapp/src/App.js
@@ -1,31 +1,25 @@
-"use strict";
-// Import Components
-import React, { Component } from 'react';
-import Navigation from './Navigation';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText
-} from 'reactstrap';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-
-    render() {
-      return <Navigation></Navigation>
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
---
+++ informapp/src/Courses.js
@@ -0,0 +1,2 @@
+'use strict';
+// Ian and Leon
