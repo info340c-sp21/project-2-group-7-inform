43aac45707883c87c7644f3c654867f14f77729a
Catherine Miao <md0615@uw.edu>
2021-05-26 16:07:37 -0700

    complete footer & nav

--- informapp/src/Footer.js
+++ informapp/src/Footer.js
@@ -1,2 +1,21 @@
 'use strict';
 // Daniel & Catherine
+import React from 'react';
+import './style.css'
+import { Icon, InlineIcon } from '@iconify/react';
+import paperPlaneO from '@iconify-icons/fa/paper-plane-o';
+
+
+export default function Footer() {
+  return(
+      <footer>
+      <div className="footer-content">
+        <p className="text-center">Copyright &copy; 2021 Leon & Ian & Catherine & Daniel</p>
+        <p className="text-center">
+          <Icon icon={paperPlaneO} />
+          <a href="mailto:leonykan@uw.edu" className="text-white">Contact Us</a>
+        </p>
+      </div>
+    </footer>
+  );
+}
